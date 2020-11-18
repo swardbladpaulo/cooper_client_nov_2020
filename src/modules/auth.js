3bf41f40284ebc3e541d7d54903d9d89ad8e3c86
swardbladpaulo <paulo@swardblads.se>
2020-11-19 00:08:00 +0100

    small modified

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -10,7 +10,7 @@ const authenticate = async (email, password) => {
     return { authenticated: true };
   } catch (error) {
     return { authenticated: false, message: error.response.data.errors[0] };
-  }
+  };
 };
 
 const storeAuthCredentials = ({ headers }) => {
