1ee0cac18ef55c99348ccbf26d8cbddc69ee41d7
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-19 16:26:05 +0100

    Fix connection to db from client. Login works with localhost. Add new users via postman

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -8,20 +8,20 @@ describe("User authenticates", () => {
   it("successfully with valid credentials", () => {
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
+      cy.get("#email").type("erik@erik6.se");
       cy.get("#password").type("password");
       cy.get('button').contains('Submit').click()
     });
-    cy.get("#message").should("contain", "Hi user@mail.com");
+    cy.get("#message").should("contain", "Hi erik@erik6.se");
   });
 
   it("unsuccessfully with invalid credentials", () => {
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
+      cy.get("#email").type("erik@erik6.se");
       cy.get("#password").type("wrongpassword");
       cy.get('button').contains('Submit').click()
     });
-    cy.get("#message").should("contain", "Invalid login credentials, Please try again.");
+    cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable default-case */
+/* eslint-disable no-undef */
 import React, { Component } from "react";
+
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
-import LoginForm from './components/LoginForm';
-import { authenticate } from './modules/auth';
+import LoginForm from "./components/LoginForm";
+import { authenticate } from "./modules/auth";
 
 class App extends Component {
   state = {
@@ -11,14 +14,14 @@ class App extends Component {
     age: "",
     renderLoginForm: false,
     authenticated: false,
-    message: ""
+    message: "",
   };
 
-  onChangeHandler = e => {
+  onChangeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onLogin = async e => {
+  onLogin = async (e) => {
     e.preventDefault();
     const response = await authenticate(
       e.target.email.value,
@@ -34,8 +37,7 @@ class App extends Component {
   render() {
     const { renderLoginForm, authenticated, message } = this.state;
     let renderLogin;
-    // eslint-disable-next-line
-    switch(true) {
+    switch (true) {
       case renderLoginForm && !authenticated:
         renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
         break;
@@ -48,30 +50,27 @@ class App extends Component {
             >
               Login
             </button>
-            <p>{message}</p>
+            <p id="message">{message}</p>
           </>
         );
         break;
       case authenticated:
         renderLogin = (
-          <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+          <p id="message">Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
         );
         break;
-    };
-
-      return (
+    }
+    return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
-        {renderLogin}
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
+        {renderLogin}
       </>
     );
   }
 }
-
 export default App;
-
