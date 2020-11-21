3725abed317a07ed72f7b80df51cb7e7ac797434
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-21 14:50:31 +0100

    Adding a doughnut chart for displaying  previous tests

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getData } from "../modules/performanceData";
-import { Line } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 
 class DisplayPerformanceData extends Component {
   state = {
@@ -31,37 +31,40 @@ class DisplayPerformanceData extends Component {
 
     if (this.state.performanceData != null) {
       this.state.performanceData.forEach((entry) => {
-        distances.push(entry.data.distance)
-        labels.push(entry.data.message)
+        distances.push(entry.data.distance);
+        labels.push(entry.data.message);
       });
-      
-      // (
-      //   <div>
-      //     {this.state.performanceData.map((item) => {
-      //       return (
-      //         <div key={item.id}>
-      //           <p>{item.data.message}</p>
-      //           <p>{item.data.age}</p>
-      //           <p>{item.data.distance}</p>
-      //         </div>
-      //       );
-      //     })}
-      //   </div>
-      // );
     }
 
     const data = {
       labels: labels,
-      datasets: [ {
-        label: "Previous results",
-        data: distances
-      }],
-    }
+      datasets: [
+        {
+          // label: "Fitness level",
+          data: distances,
+        },
+      ],
+    };
 
-    graph = <Line data={data}/>;
+    graph = <Doughnut data={data} />;
 
-    return <div id="index">{graph}</div>;
+    return <div id="index">
+    <h1 className='title'>Accumulated test results</h1>{graph}</div>;
   }
 }
 
 export default DisplayPerformanceData;
+
+// (
+//   <div>
+//     {this.state.performanceData.map((item) => {
+//       return (
+//         <div key={item.id}>
+//           <p>{item.data.message}</p>
+//           <p>{item.data.age}</p>
+//           <p>{item.data.distance}</p>
+//         </div>
+//       );
+//     })}
+//   </div>
+// );
