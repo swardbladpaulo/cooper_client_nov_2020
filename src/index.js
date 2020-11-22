ab357cbdadd8efd0cdcbcdd62b7f1cff27cc4ab8
swardbladpaulo <paulo@swardblads.se>
2020-11-22 21:30:47 +0100

    typs and refactor

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import axios from 'axios';
 import 'semantic-ui-css/semantic.min.css';
 
 let apiUrl;
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
   apiUrl = "https://sp-cooper-api.herokuapp.com/api/v1";
-} else {
-  apiUrl = "http://localhost:3000/api/v1";
-}
+//} else {
+//  apiUrl = "http://localhost:3000/api/v1";
+//}
 axios.defaults.baseURL = apiUrl;
 
 ReactDOM.render(<App />, document.getElementById("root"));
